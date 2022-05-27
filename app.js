1ef1f6f286971eeb4744f79b38abea5480e1465b
MohamedGouaouri <im_gouaouri@esi.dz>
2022-05-28 00:28:57 +0100

    routes docs added

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
+
 var app = express();
 
 require('dotenv').config()
@@ -21,15 +24,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Codeground grading service API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./routes/*.js'], // files containing annotations as above
+};
+
+const swaggerDocs = swaggerJsDoc(options);
+
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs))
+
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -6,65 +6,110 @@ var sendResponse = require("../Utils").sendResponse
 var fastestStrategy = require("../Utils").fastestStrategy;
 var mostOptimizedStrategy = require("../Utils").mostOptimizedStrategy;
 var numberOfTestPassedStrategy = require("../Utils").numberOfTestPassedStrategy;
-/* GET home page. */
-router.post('/api/submit', function(req, res, next) {
-    const rce_request_body={
-      "challenge_id" : req.body["challenge_id"],
-      code : req.body.code,
-      language : req.body.language
+
+
+
+/**
+ * @swagger
+ *  /api/submit:
+ *    post:
+ *      summary: Submits users code
+ *      description: Submits users code for a specific challenge
+ *      consumes:
+ *        application/json
+ *      produces:
+ *        application/json
+ *      parameters:
+ *       - in: body
+ *         name: body
+ *         required: true
+ *         description: Room payload
+ *         schema:
+ *          type: object
+ *          properties:
+ *            challenge_id:
+ *              type: string
+ *              description: The challenge ID
+ *              example: "challenge_fe888w4561W"
+ *            code:
+ *              type: string
+ *              desription: User supplied code
+ *            language:
+ *              type: string
+ *              description: The programming language the code is written in
+ *              example: python
+ * 
+ *      responses:
+ *        200:
+ *          description: Success, sends score
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  code:
+ *                    type: integer,
+ *                    description: Submission score
+ *                    example: 20
+ */
+router.post('/api/submit', function (req, res, next) {
+    const rce_request_body = {
+        "challenge_id": req.body["challenge_id"],
+        code: req.body.code,
+        language: req.body.language
     }
 
     const strategyToUse = req.body.strategy
-    const secondsLeft= req.body.secondsLeft
+    const secondsLeft = req.body.secondsLeft
 
     const rce_request_options = {
         // Pour athentification
         // Authorization: `Bearer ${localStorage.getItem("gacela-token")}`,
         // pour specifier le format de reponse
-        headers : { "Content-Type": "application/json"}
+        headers: { "Content-Type": "application/json" }
     };
 
-    axios.post(process.env.RCE_ENDPOINT  , rce_request_body , rce_request_headers )
-        .then(resp=>{
-    //         resp={
-    //             test_results : [
-    //                 {
-    //                     test_id : 1,
-    //                     status : "passed",
-    //                     time : 0.2
-    //                 },
-    //                 {
-    //                     test_id : 2,
-    //                     status : "failed",
-    //                     time : 0.5
-    //                 },
-    //                 {
-    //                     test_id : 3,
-    //                     status : "passed",
-    //                     time : 0.7
-    //                 },
-    //
-    //             ]
-    //         }
+    axios.post(process.env.RCE_ENDPOINT, rce_request_body, rce_request_headers)
+        .then(resp => {
+            //         resp={
+            //             test_results : [
+            //                 {
+            //                     test_id : 1,
+            //                     status : "passed",
+            //                     time : 0.2
+            //                 },
+            //                 {
+            //                     test_id : 2,
+            //                     status : "failed",
+            //                     time : 0.5
+            //                 },
+            //                 {
+            //                     test_id : 3,
+            //                     status : "passed",
+            //                     time : 0.7
+            //                 },
+            //
+            //             ]
+            //         }
             let TheResultedScore;
-            switch (strategyToUse){
+            switch (strategyToUse) {
                 case "FASTEST":
-                    TheResultedScore=fastestStrategy({testResults : resp["test_results"] , timeLeft : secondsLeft})
+                    TheResultedScore = fastestStrategy({ testResults: resp["test_results"], timeLeft: secondsLeft })
                     break;
                 case "OPTIMIZED":
-                    TheResultedScore=mostOptimizedStrategy({testResults :resp["test_results"] })
+                    TheResultedScore = mostOptimizedStrategy({ testResults: resp["test_results"] })
                     break;
                 case "SUCCESS_RATIO":
-                    TheResultedScore= numberOfTestPassedStrategy({testResults :resp["test_results"] })
+                    TheResultedScore = numberOfTestPassedStrategy({ testResults: resp["test_results"] })
                     break;
-                default :
+                default:
                     throw new Error("UNKNOWN Test STRATEGY !");
             }
 
-            sendResponse(res , 200 , true , {score : TheResultedScore , calculatedAt : new Date()} )
+            sendResponse(res, 200, true, { score: TheResultedScore, calculatedAt: new Date() })
         })
-        .catch(err=>{
-            sendResponse(res , 500 , false , err.message)
+        .catch(err => {
+            sendResponse(res, 500, false, err.message)
         })
 
 
